e86d11aa91233559b5b833c476092feac526ff86
TechnologicalJerry <cserajatverma@gmail.com>
2025-06-22 21:57:40 +0530

    Update User.ts

---
+++ real-time-chat-application/src/models/User.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+export interface IUser extends mongoose.Document {
+    username: string;
+    password: string;
+}
+const UserSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true }
+});
+export default mongoose.model<IUser>('User', UserSchema);
