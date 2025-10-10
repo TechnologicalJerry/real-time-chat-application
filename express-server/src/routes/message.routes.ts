0470d62907f17d5c985f4cc74d6328caabe58577
TechnologicalJerry <cserajatverma@gmail.com>
2025-10-10 18:46:07 +0530

    Remove message routes file
    Deleted message.routes.ts from the routes directory, removing the message-related API endpoint and its associated middleware. This may be part of a refactor or feature deprecation.

--- express-server/src/routes/message.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { getMessages } from '../controllers/message.controller';
-import { authMiddleware } from '../middleware/auth.middleware';
-
-const router = Router();
-router.get('/', authMiddleware, getMessages);
-export default router;
