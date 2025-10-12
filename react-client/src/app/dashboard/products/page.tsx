532dec3fa3cacd980e5fe0926b1884c7947747fe
TechnologicalJerry <cserajatverma@gmail.com>
2025-10-12 20:13:17 +0530

    Add products dashboard page
    Introduces the ProductsPage component to render the ProductsList in the dashboard. This sets up the products section for the dashboard in the React client.

---
+++ react-client/src/app/dashboard/products/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+import { ProductsList } from '@/components/dashboard/Products/ProductsList';
+
+export default function ProductsPage() {
+  return <ProductsList />;
+}
+
