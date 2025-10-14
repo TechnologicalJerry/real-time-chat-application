5b00f8ced2b4394f10ccaa3e15ba14fc57433550
TechnologicalJerry <cserajatverma@gmail.com>
2025-10-14 15:56:21 +0530

    Add useSocket custom hook
    Introduces a useSocket hook as a wrapper around useSocketContext for easier access to socket functionality within components.

---
+++ react-client/src/lib/hooks/useSocket.ts
@@ -0,0 +1,11 @@
+import { useSocketContext } from '../context/SocketContext';
+
+/**
+ * Custom hook for socket functionality
+ * Wrapper around SocketContext for easier access
+ */
+export const useSocket = () => {
+  return useSocketContext();
+};
+
+
