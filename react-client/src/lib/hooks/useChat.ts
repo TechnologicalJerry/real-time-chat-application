3b7d9686b1d998641ea1197f71497347d55cd99a
TechnologicalJerry <cserajatverma@gmail.com>
2025-10-14 15:56:06 +0530

    Add useChat custom hook for ChatContext access
    Introduces a useChat hook that wraps useChatContext, providing a simpler interface for accessing chat functionality within components.

---
+++ react-client/src/lib/hooks/useChat.ts
@@ -0,0 +1,11 @@
+import { useChatContext } from '../context/ChatContext';
+
+/**
+ * Custom hook for chat functionality
+ * Wrapper around ChatContext for easier access
+ */
+export const useChat = () => {
+  return useChatContext();
+};
+
+
