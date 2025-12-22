fe240da5093792ad9bc60760fe81c558ef209f4d
TechnologicalJerry <cserajatverma@gmail.com>
2025-12-22 18:10:26 +0530

    Add initial Angular Ionic and React Native client configs
    Added configuration and setup files for both Angular Ionic and React Native client applications, including editor, linter, build, and project settings. This establishes the project structure and development environment for both platforms.

---
+++ angular-ionic-client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'real-time-chat-application',
+  webDir: 'www'
+};
+
+export default config;
